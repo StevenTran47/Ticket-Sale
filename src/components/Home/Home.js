3961c46bc80ccb0350d9cb59a10f2e0f72086971
StevenTran47 <hoangtran32357000@gmail.com>
2022-02-16 12:57:00 +0700

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,37 @@
 import React from "react"
+import '../../assets/css/home.css'
 
 const Home = () => {
+    
     return (
             <div className="content">
                 <h1>Thống kê</h1>
+                    <div className="chart">
+                    <h3>Doanh thu</h3>
+                        <div className="chart-number-total">
+                            <p>260tr</p>
+                            <p>220tr</p>
+                            <p>180tr</p>
+                            <p>140tr</p>
+                        </div>  
+                        <div className="chart-shape">
+                            <img style={{position:'relative',width:'1000px',top:'28px',paddingLeft:'50px'}} src="./img/Line.png"></img>
+                            <img style={{position:'relative',width:'1000px',top:'70px',paddingLeft:'50px'}} src="./img/Line.png"></img>
+                            <img style={{position:'relative',width:'1000px',top:'110px',paddingLeft:'50px'}} src="./img/Line.png"></img>
+                            <img style={{position:'relative',width:'1000px',top:'150px',paddingLeft:'50px'}} src="./img/Line.png"></img>
+                            <img style={{position:'relative',width:'1000px',top:'6px',paddingLeft:'50px'}} src="./img/Shape-1.png"></img>
+                            <img style={{position:'relative',width:'1000px',top:'-140px',paddingLeft:'50px'}} src="./img/Shape-2.png"></img>
+                        </div>
+                        <div className="chart-day-total">
+                            <p>Thứ 2</p>
+                            <p>Thứ 3</p>
+                            <p>Thứ 4</p>
+                            <p>Thứ 5</p>
+                            <p>Thứ 6</p>
+                            <p>Thứ 7</p>
+                            <p>CN</p>
+                        </div>
+                    </div>
             </div>
     )
 }
