5a9ceb97f2545154e4262d8b5d15bca4ff53cb9e
StevenTran47 <hoangtran32357000@gmail.com>
2022-02-17 12:02:43 +0700

    upload chart

--- src/components/Home/chart.js
+++ src/components/Home/chart.js
@@ -1,34 +1,45 @@
-import React from "react"
+import { Line } from "@ant-design/charts"
+import React, { useEffect, useState } from "react"
 import '../../assets/css/chart.css'
 
 
 function chart()  {
+  const ChartLine = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        asyncFetch();
+    }, []);
+
+    const asyncFetch = () => {
+        fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => {
+                console.log('fetch data failed', error);
+            });
+    };
+    const config = {
+        data,
+        padding: 40,
+        xField: 'Date',
+        yField: 'scales',
+        xAxis: {
+          range: [0, 1],
+          tickCount: 5,
+        },
+        areaStyle: () => {
+          return {
+            fill: 'l(270) 0:#ffffff 1:#FAA05F ',
+          };
+        },
+    };
+    
+    return <Line {...config} />;
+};
   return (
     <div className="chart">
-    <h3>Doanh thu</h3>
-        <div className="chart-number-total">
-            <p>260tr</p>
-            <p>220tr</p>
-            <p>180tr</p>
-            <p>140tr</p>
-        </div>  
-        <div className="chart-shape">
-            <img style={{position:'relative',width:'1100px',top:'28px',paddingLeft:'50px'}} src="./img/Line.png"></img>
-            <img style={{position:'relative',width:'1100px',top:'70px',paddingLeft:'50px'}} src="./img/Line.png"></img>
-            <img style={{position:'relative',width:'1100px',top:'110px',paddingLeft:'50px'}} src="./img/Line.png"></img>
-            <img style={{position:'relative',width:'1100px',top:'150px',paddingLeft:'50px'}} src="./img/Line.png"></img>
-            <img style={{position:'relative',width:'1100px',top:'-6px',paddingLeft:'50px'}} src="./img/Shape-1.png"></img>
-            <img style={{position:'relative',width:'1100px',top:'-165px',paddingLeft:'50px'}} src="./img/Shape-2.png"></img>
-        </div>
-        <div className="chart-day-total">
-            <p>Thứ 2</p>
-            <p>Thứ 3</p>
-            <p>Thứ 4</p>
-            <p>Thứ 5</p>
-            <p>Thứ 6</p>
-            <p>Thứ 7</p>
-            <p>CN</p>
-        </div>
+      <ChartLine />    
     </div>
   )
 }
